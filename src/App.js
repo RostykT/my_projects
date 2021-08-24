a04058669f37595b3a48ac75f9494184dffed354
Rostyk <rostishka1@gmail.com>
2021-08-24 19:53:30 +0300

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import Header from "./components/Header";
+import Home from "./components/Home";
 import "./App.css";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Header />
+			<Home />
+		</div>
+	);
 }
 
 export default App;
