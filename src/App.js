48d7bc299a65ed29251d66c807f2a79fa427f12a
Rostyk <rostishka1@gmail.com>
2021-08-31 22:59:15 +0300

    add all dependencies, styled NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
-
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 function App() {
 	return (
 		<div className="App">
-			<h1>Portfolio</h1>
+			<Router>
+				<Navbar />
+			</Router>
 		</div>
 	);
 }
