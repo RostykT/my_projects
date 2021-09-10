fd7692db182c3c270ddc0cb26f8c008ae5a01851
Rostyk <rostishka1@gmail.com>
2021-09-10 21:49:14 +0300

    add focus on input area, via hooks useEffect and useRef

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function TodoForm(props) {
 	const [input, SetInput] = useState("");
+
+	const inputRef = useRef(null);
+	useEffect(() => {
+		inputRef.current.focus();
+	});
 
 	const handleChange = (e) => {
 		SetInput(e.target.value);
@@ -25,6 +30,7 @@ function TodoForm(props) {
 				name="text"
 				className="todo-input"
 				onChange={handleChange}
+				ref={inputRef}
 			/>
 			<button className="todo-button"> Add todo</button>
 		</form>
